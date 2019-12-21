af77ed33ad2b4bd5eaa79e4f9de48d6b02a19b8d
姚梦云 <yaomengyun@hztianque.com>
2019-12-21 21:04:59 +0800

    动态路由demo

--- src/router/router.js
+++ src/router/router.js
@@ -14,6 +14,11 @@ export default [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+  },
+  {
+    // name 动态路由参数
+    path: '/argu/:name',
+    component: () => import('@/views/argu.vue')
   }
 ]
